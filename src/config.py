a248abed5866e07cace8eeb68f6f3eaf472a9c08
rlaengud123 <53484692+rlaengud123@users.noreply.github.com>
2020-11-17 01:14:27 +0900

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,6 @@
+COCO_CLASSES = ['small ship', 'large ship', 'civilian aircraft', 'military aircraft', 'small car', 'bus', 'truck', 'train',
+        'crane', 'bridge', 'oil tank', 'dam', 'athletic field', 'helipad', 'roundabout','etc','background']
+
+colors = [(39, 129, 113), (164, 80, 133), (83, 122, 114), (99, 81, 172), (95, 56, 104), (37, 84, 86), (14, 89, 122),
+          (80, 7, 65), (10, 102, 25), (90, 185, 109), (106, 110, 132), (169, 158, 85), (188, 185, 26), (103, 1, 17),
+          (82, 144, 81), (92, 7, 184), (122, 37, 185)]
